349f7b1826824ec382dc4817836c1f982118cebb
mirkamol <mirkamol@gmail.com>
2024-05-06 16:31:27 +0500

    adding update function to serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -31,3 +31,11 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.get('password')
+        user = super().update(instance, validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
